455239cfdbe2166c1429ec4f7e79b152d6ad2249
Nathan <nathan.dsouza316@gmail.com>
2025-06-28 15:41:58 +1200

    Add test endpoint Add strict transport security

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 import securescaffold
 from flask import render_template, send_from_directory
+from securescaffold.contrib.appengine import users
 
 app = securescaffold.create_app(__name__)
 
@@ -19,3 +20,16 @@ def favicon():
         "favicon.ico",
         mimetype="image/vnd.microsoft.icon",
     )
+
+
+@app.route("/test")
+def testauth():
+    user = users.get_current_user()
+
+    if user:
+        email = user.email()
+        user_id = user.user_id()
+
+        return f"Hello signed-in user {email} {user_id}"
+    
+    return "Not signed-in"
